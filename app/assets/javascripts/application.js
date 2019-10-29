67b480c32bd1dd2227bb6aa02b03cd6ab715f51b
Ryoga <e175749@ie.u-ryukyu.ac.jp>
2019-10-29 18:32:15 +0900

    バグ修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,35 +7,35 @@
 //= require popper
 //= require bootstrap-sprockets
 
-window.onload = function () {
-    this.msg_changer();
-}
+// window.onload = function () {
+//     this.msg_changer();
+// }
 
-function msg_changer() {
-    var now = new Date();
-    var hour = now.getHours();
+// function msg_changer() {
+//     var now = new Date();
+//     var hour = now.getHours();
 
-    if(hour >= 5 && hour <= 8){
-        // var msg_content = document.getElementById("msg");
-        // msg_content.innerHTML = '5-8';
-    }
-    else if(hour >= 9 && hour <= 11){
-        var msg_content = document.getElementById("msg");
-        msg_content.innerHTML = '9-11';
-        document.getElementById("msg-txt").classList.value = "haraheri-msg";
-    }
-    else if(hour >= 12 && hour <= 13){
-        // var msg_content = document.getElementById("msg");
-        document.getElementById("msg-txt").classList.value = "hello-msg";
-    }
-    else if(hour >= 14 && hour <= 17){
-        // var msg_content = document.getElementById("msg");
-        // msg_content.innerHTML = '14-17';
-        document.getElementById("msg-txt").classList.value = "daijoubu-msg";
-    }
-    else if(hour >= 18 && hour <= 23){
-        // var msg_content = document.getElementById("msg");
-        // msg_content.innerHTML = '18-23';
-        document.getElementById("msg-txt").classList.value = "hello-msg";
-    }
-}
+//     if(hour >= 5 && hour <= 8){
+//         // var msg_content = document.getElementById("msg");
+//         // msg_content.innerHTML = '5-8';
+//     }
+//     else if(hour >= 9 && hour <= 11){
+//         var msg_content = document.getElementById("msg");
+//         msg_content.innerHTML = '9-11';
+//         document.getElementById("msg-txt").classList.value = "haraheri-msg";
+//     }
+//     else if(hour >= 12 && hour <= 13){
+//         // var msg_content = document.getElementById("msg");
+//         document.getElementById("msg-txt").classList.value = "hello-msg";
+//     }
+//     else if(hour >= 14 && hour <= 17){
+//         // var msg_content = document.getElementById("msg");
+//         // msg_content.innerHTML = '14-17';
+//         document.getElementById("msg-txt").classList.value = "daijoubu-msg";
+//     }
+//     else if(hour >= 18 && hour <= 23){
+//         // var msg_content = document.getElementById("msg");
+//         // msg_content.innerHTML = '18-23';
+//         document.getElementById("msg-txt").classList.value = "hello-msg";
+//     }
+// }
--- app/assets/javascripts/watch.js
+++ app/assets/javascripts/watch.js
@@ -1,98 +1,95 @@
+// // select要素の取得
+// var selectMinute = document.timer.minute;
+// var selectSecond = document.timer.second;
 
-// select要素の取得
-var selectMinute = document.timer.minute;
-var selectSecond = document.timer.second;
-
-var indexMinute =  selectMinute.selectedIndex;
-var indexSecond =  selectSecond.selectedIndex;
-var minuteData = selectMinute.options[ indexMinute ].innerHTML;
-var secondData = selectSecond.options[ indexSecond ].innerHTML;
-
-
-var timer1; //タイマーを格納する変数（タイマーID）の宣言
-
-
-//カウントダウン関数を1000ミリ秒毎に呼び出す関数
-function cntStart()
-{
-    document.timer.elements[0].disabled=true;
-    document.timer.elements[1].disabled=true;
-    document.timer.elements[2].disabled=true;
-    document.timer.elements[4].disabled=true;
-    indexMinute =  selectMinute.selectedIndex;
-    indexSecond =  selectSecond.selectedIndex;
-    minuteData = selectMinute.options[ indexMinute ].innerHTML;
-    secondData = selectSecond.options[ indexSecond ].innerHTML;
-    timer1=setInterval("countDown()",1000);
-}
-
-//タイマー停止関数
-function cntStop()
-{
-    document.timer.elements[2].disabled=false;
-    document.timer.elements[4].disabled=false;
-    clearInterval(timer1);
-}
-
-//カウントダウン関数
-function countDown()
-{
-    min=minuteData;
-    sec=secondData;
-
-    if( (min==0) && (sec==0) )
-    {
-        alert("時刻を設定してください！");
-        reSet();
-    }
-    else
-    {
-        if (min=="") min=0;
-        min=parseInt(min);
-
-        if (sec=="") sec=0;
-        sec=parseInt(sec);
-
-        tmWrite(min*60+sec-1);
-    }
-}
-
-//残り時間を書き出す関数
-function tmWrite(int)
-{
-    int=parseInt(int);
-
-    if (int<=0)
-    {
-        reSet();
-        alert("時間です！");
-    }
-    else
-    {
-        //残り分数はintを60で割って切り捨てる
-        minuteData=Math.floor(int/60);
-        //残り秒数はintを60で割った余り
-        secondData=int % 60;
-
-        if (minuteData < 10 && secondData < 10) {
-            document.getElementById('timeDisplay').innerHTML = "0" + String(minuteData) + ":" + "0" + String(secondData);
-        }else if (minuteData < 10) {
-            document.getElementById('timeDisplay').innerHTML = "0" + String(minuteData) + ":" + String(secondData);
-        }else if (secondData < 10) {
-            document.getElementById('timeDisplay').innerHTML = String(minuteData) + ":" + "0" + String(secondData);
-        }else{
-            document.getElementById('timeDisplay').innerHTML = String(minuteData) + ":" + String(secondData);
-        }
-    }
-}
-
-//フォームを初期状態に戻す（リセット）関数
-function reSet()
-{
-    document.timer.elements[0].disabled=false;
-    document.timer.elements[1].disabled=false;
-    document.timer.elements[2].disabled=false;
-    document.timer.elements[4].disabled=false;
-    document.getElementById('timeDisplay').innerHTML = "00:00" ;
-    clearInterval(timer1);
-}
+// var indexMinute =  selectMinute.selectedIndex;
+// var indexSecond =  selectSecond.selectedIndex;
+// var minuteData = selectMinute.options[ indexMinute ].innerHTML;
+// var secondData = selectSecond.options[ indexSecond ].innerHTML;
+// //select要素のchangeイベントの登録
+// selectMinute.addEventListener('change', function(){
+//         //選択されたoption番号を取得
+//         indexMinute =  this.selectedIndex;
+//         minuteData = this.options[ indexMinute ].innerHTML;
+// });
+// selectSecond.addEventListener('change', function(){
+//         //選択されたoption番号を取得
+//         indexSecond =  this.selectedIndex;
+//         secondData = this.options[ indexSecond ].innerHTML;
+// });
+// var timer1; //タイマーを格納する変数（タイマーID）の宣言
+// //カウントダウン関数を1000ミリ秒毎に呼び出す関数
+// function cntStart()
+// {
+//     document.timer.elements[0].disabled=true;
+//     document.timer.elements[1].disabled=true;
+//     document.timer.elements[2].disabled=true;
+//     document.timer.elements[4].disabled=true;
+//     timer1=setInterval("countDown()",1000);
+// }
+// //タイマー停止関数
+// function cntStop()
+// {
+//     document.timer.elements[2].disabled=false;
+//     document.timer.elements[4].disabled=false;
+//     clearInterval(timer1);
+// }
+// //カウントダウン関数
+// function countDown()
+// {
+//     var min=minuteData;
+//     var sec=secondData;
+//     if( (min==0) && (sec==0) )
+//     {
+//         alert("時刻を設定してください！");
+//         reSet();
+//     }
+//     else
+//     {
+//         if (min=="") min=0;
+//         min=parseInt(min);
+//         if (sec=="") sec=0;
+//         sec=parseInt(sec);
+//         tmWrite(min*60+sec-1);
+//     }
+// }
+// //残り時間を書き出す関数
+// function tmWrite(int)
+// {
+//     int=parseInt(int);
+//     if (int<=0)
+//     {
+//         reSet();
+//         alert("時間です！");
+//     }
+//     else
+//     {
+//         //残り分数はintを60で割って切り捨てる
+//         minuteData=Math.floor(int/60);
+//         //残り秒数はintを60で割った余り
+//         secondData=int % 60;
+//         if (minuteData < 10 && secondData < 10) {
+//             document.getElementById('timeDisplay').innerHTML = "0" + String(minuteData) + ":" + "0" + String(secondData);
+//         }else if (minuteData < 10) {
+//             document.getElementById('timeDisplay').innerHTML = "0" + String(minuteData) + ":" + String(secondData);
+//         }else if (secondData < 10) {
+//             document.getElementById('timeDisplay').innerHTML = String(minuteData) + ":" + "0" + String(secondData);
+//         }else{
+//             document.getElementById('timeDisplay').innerHTML = String(minuteData) + ":" + String(secondData);
+//         }
+//     }
+// }
+// //フォームを初期状態に戻す（リセット）関数
+// function reSet()
+// {
+//     document.timer.elements[0].disabled=false;
+//     document.timer.elements[1].disabled=false;
+//     document.timer.elements[2].disabled=false;
+//     document.timer.elements[4].disabled=false;
+//     document.getElementById('timeDisplay').innerHTML = "00:00" ;
+//     indexMinute = selectMinute.selectedIndex;
+//     minuteData = selectMinute.options[ indexMinute ].innerHTML;
+//     indexSecond =  selectSecond.selectedIndex;
+//     secondData = secondData = selectSecond.options[ indexSecond ].innerHTML;
+//     clearInterval(timer1);
+// }
