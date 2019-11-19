ec21fd9742a0ee413c3c69b06eab2e35063251de
tmrekk121 <tmrekk121@outlook.jp>
2019-11-19 11:52:25 +0900

    headerをpartialに変更

--- app/controllers/top_pages_controller.rb
+++ app/controllers/top_pages_controller.rb
@@ -1,10 +1,19 @@
 class TopPagesController < ApplicationController
   #before_action :authenticate_user!,only: [:home]
-
+  layout 'top', only: [:top]
 
   def home
     @micropost = Micropost.all
     @micropost_new = Micropost.new
+  end
+
+  def top
+  end
+
+  def q_and_a
+  end
+
+  def sukuram_master
   end
 
   def watch
