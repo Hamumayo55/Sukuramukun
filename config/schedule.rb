4489c6b9c7a901ae8a98071634387f071bef525b
Takuya Shinjo <e175769@ie.u-ryukyu.ac.jp>
2019-12-02 15:16:58 +0900

    エラーの修正

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,11 +2,11 @@ require File.expand_path(File.dirname(__FILE__) + "/environment")
 rails_env = ENV['RAILS_ENV'] || :development
 set :environment, rails_env
 set :output, "#{Rails.root}/log/cron.log"
-set :job_template, "/bin/zsh -l -c ':job'"
+set :job_template, "/bin/zsh':job'"
 set :bundle_command, 'bundle exec'
 job_type :runner,  "cd #{Rails.root} && :bundle_command rails runner -e :environment ':task' :output"
 
 
-every 1.day, at: '1:43 pm' do
+every 1.day, at: '3:15 pm' do
     runner "SendMailer.send_mail"
 end
