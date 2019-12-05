855dc81e11c86cf385e2eb1701097e2f52201980
Takuya Shinjo <e175769@ie.u-ryukyu.ac.jp>
2019-12-05 18:20:27 +0900

    wheneverとrakeの連携

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,11 +2,13 @@ require File.expand_path(File.dirname(__FILE__) + "/environment")
 rails_env = ENV['RAILS_ENV'] || :development
 set :environment, rails_env
 set :output, "#{Rails.root}/log/cron.log"
-set :job_template, "/bin/zsh':job'"
+set :job_template, "/bin/zsh -l -c':job'"
 set :bundle_command, 'bundle exec'
 job_type :runner,  "cd #{Rails.root} && :bundle_command rails runner -e :environment ':task' :output"
+job_type :rake, "cd #{Rails.root} :environment_variable=:environment bundle exec rake :task :output"
 
-
-every 1.day, at: '6:25 pm' do
-    runner "SendMailer.send_mail"
+every 1.day, at: '3:55 pm' do
+    rake "send_email:send"
 end
+
+
